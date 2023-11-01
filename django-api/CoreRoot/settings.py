4a9245ae9172483a942b03b0413ff2ee6df0de58
nextwarrior000 <nextwarrior000@gmail.com>
2023-11-01 06:55:06 +0100

    🎨 fix: add avatar field to user model

--- django-api/CoreRoot/settings.py
+++ django-api/CoreRoot/settings.py
@@ -133,6 +133,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+# Default avatar URL
+
+DEFAULT_AVATAR_URL = "https://avatars.dicebear.com/api/identicon/.svg"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- django-api/core/user/serializers.py
+++ django-api/core/user/serializers.py
@@ -1,9 +1,22 @@
 from core.abstract import AbstractSerializer
 from rest_framework import serializers
 from core.user.models import User
+from django.conf import settings
 
 
 class UserSerializer(AbstractSerializer):
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if not representation["avatar"]:
+            representation["avatar"] = settings.DEFAULT_AVATAR_URL
+            return representation
+        if settings.DEBUG:  # debug enabled for dev
+            request = self.context.get("request")
+            representation["avatar"] = request.build_absolute_uri(
+                representation["avatar"]
+            )
+        return representation
 
     def update(self, instance, validated_data):
 
@@ -13,6 +26,6 @@ class UserSerializer(AbstractSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name',
-                  'last_name', 'bio', 'email',
+                  'last_name', 'bio', 'email', 'avatar',
                   'is_active', 'created', 'updated']
         read_only_field = ['is_active']
