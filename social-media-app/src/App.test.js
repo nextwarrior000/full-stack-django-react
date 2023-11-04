96406818d77e07f9ad8961fec932c5e187791574
nextwarrior000 <nextwarrior000@gmail.com>
2023-11-04 21:20:13 +0100

    🎨 feat: add test

--- social-media-app/src/App.test.js
+++ social-media-app/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+test("renders Welcome to Postagram text", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const textElement = screen.getByText(/Welcome to Postagram!/i);
+  expect(textElement).toBeInTheDocument();
 });
